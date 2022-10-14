92ba1a70b4a7b59225117d2edf0fca388be2e65c
Nastena3091 <nastena20043091@gmail.com>
2022-10-14 15:26:35 +0300

    ПР 4_2

--- Delivery club/js/main.js
+++ Delivery club/js/main.js
@@ -13,17 +13,25 @@ new WOW().init();
 
 ///day1
 
-const buttonAuth = document.querySelector('.button-auth');
-const modalAuth = document.querySelector('.modal-auth');
-const closeAuth = document.querySelector('.close-auth');
-const logInForm = document.querySelector('#logInForm');
-const loginInput = document.querySelector('#login');
-const userName = document.querySelector('.user-name');
-const buttonOut = document.querySelector('.button-out');
+const buttonAuth = document.querySelector(".button-auth");
+const modalAuth = document.querySelector(".modal-auth");
+const closeAuth = document.querySelector(".close-auth");
+const logInForm = document.querySelector("#logInForm");
+const loginInput = document.querySelector("#login");
+const passwordInput = document.querySelector("#password");
+const userName = document.querySelector(".user-name");
+const buttonOut = document.querySelector(".button-out");
 
 let login = localStorage.getItem('gloDelivery');
 function toogleModalAuth() {
-    modalAuth.classList.toggle("is-open")
+    modalAuth.classList.toggle("is-open");
+    if (modalAuth.classList.contains("is-open")) {
+        disabledScroll();
+      } else {
+        enableScroll();
+      }
+      loginInput.style.borderColor = "";
+      passwordInput.style.borderColor = "";
 }
 
 
@@ -52,22 +60,29 @@ function autorized () {
 }
 function notAutorized () {
     function logIn (event) {
-        if(loginInput.value)
-        {
-            event.preventDefault();
+        event.preventDefault();
+        if (loginInput.value && passwordInput.value) {
             login = loginInput.value;
-
-            localStorage.setItem('gloDelivery', login);
+            localStorage.setItem("gloDelivery", login);
             toogleModalAuth();
-            buttonAuth.removeEventListener('click', toogleModalAuth);
-            closeAuth.removeEventListener('click', toogleModalAuth);
-            logInForm.removeEventListener('submit', logIn);
+            buttonAuth.removeEventListener("click", toogleModalAuth);
+            closeAuth.removeEventListener("click", toogleModalAuth);
+            logInForm.removeEventListener("submit", logIn);
             logInForm.reset();
             checkAuth();
-        } else{
-            alert("Логін не введено");
-            event.preventDefault();
-            toogleModalAuth();
+        } else if (loginInput.value && !passwordInput.value) {
+            loginInput.style.borderColor = "";
+            passwordInput.style.borderColor = "#ff0000";
+            passwordInput.value = "";
+        } else if (!loginInput.value && passwordInput.value) {
+            passwordInput.style.borderColor = "";
+            loginInput.style.borderColor = "#ff0000";
+            loginInput.value = "";
+        } else {
+            loginInput.style.borderColor = "#ff0000";
+            loginInput.value = "";
+            passwordInput.style.borderColor = "#ff0000";
+            passwordInput.value = "";
         }
     }
     buttonOut.style.display = "none";
@@ -76,6 +91,11 @@ function notAutorized () {
     buttonAuth.addEventListener('click', toogleModalAuth);
     closeAuth.addEventListener('click', toogleModalAuth);
     logInForm.addEventListener('submit', logIn);
+    modalAuth.addEventListener("click", function (event) {
+        if (event.target.classList.contains("is-open")) {
+          toogleModalAuth();
+        }
+      });
 }
 function checkAuth() {
     if(login){
